b6644a76e4eff5fec980a9dbf4d4c6cf1aa377c8
Sid <sid.data.universe@gmail.com>
2024-01-11 17:46:00 -0800

    Increase miner bucket count limit to 250k.

--- common/constants.py
+++ common/constants.py
@@ -6,7 +6,7 @@ from . import utils
 # How big any one data entity bucket can be to limit size over the wire.
 DATA_ENTITY_BUCKET_SIZE_LIMIT_BYTES = utils.mb_to_bytes(128)
 # How many data entity buckets any one miner index can have to limit necessary storage on the validators.
-DATA_ENTITY_BUCKET_COUNT_LIMIT_PER_MINER_INDEX = 200_000
+DATA_ENTITY_BUCKET_COUNT_LIMIT_PER_MINER_INDEX = 250_000
 # How old a data entity bucket can be before the validators do not assign any value for them.
 DATA_ENTITY_BUCKET_AGE_LIMIT_DAYS = 30
 
