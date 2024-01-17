82197ac6ae542ab775ab60cc4251177308f1f5c6
Sid <sid.data.universe@gmail.com>
2024-01-17 08:35:17 -0800

    Update python requirement to 3.10.

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     include_package_data=True,
     author_email="sid.data.universe@gmail.com",
     license="MIT",
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     install_requires=requirements,
     classifiers=[
         "Development Status :: 3 - Alpha",
