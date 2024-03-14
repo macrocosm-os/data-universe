714cfc9a9528139e41df49e9c70d3965f674d4aa
Sid <sid.data.universe@gmail.com>
2024-03-13 20:50:34 -0700

    Add datadog metrics
    Also emit evaluation_frequency_seconds.
    
    Actually only emit when 38 in batch.
    
    At statsd.timed for requests to miners.
    
    Add additional sleep before attempting to join in eval loop.
    
    Up to 300 seconds.
    
    Switch to construct for the data value calculator to skip validations.
    
    Fix bad merge

--- scraping/x/utils.py
+++ scraping/x/utils.py
@@ -9,6 +9,8 @@ from scraping.scraper import ValidationResult
 
 from scraping.x.model import XContent
 
+from datadog import statsd
+
 
 def is_valid_twitter_url(url: str) -> bool:
     """Verifies a URL is both a valid URL and is for twitter.com."""
@@ -93,7 +95,7 @@ def validate_tweet_content(
             and len(tweet_to_verify.text) > 180
             and actual_tweet.text.startswith(tweet_to_verify.text[:-1])
         ):
-            pass
+            statsd.increment("scraping.x.validate_tweet_content.ellipsis")
         else:
             bt.logging.info(
                 f"Tweet texts do not match: {tweet_to_verify} != {actual_tweet}."
