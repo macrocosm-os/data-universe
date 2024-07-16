1c8cdf3e62be22ca83fe10d795474c37242e6138
arrmlet <trubavolodymyr@gmail.com>
2024-07-16 14:31:23 +0300

    Refactor ( remove the constants related to is_retweets).

--- common/constants.py
+++ common/constants.py
@@ -32,9 +32,3 @@ MIN_EVALUATION_PERIOD = datetime.timedelta(minutes=60)
 
 # Miner compressed index cache freshness.
 MINER_CACHE_FRESHNESS = datetime.timedelta(minutes=20)
-RETWEET_ELIGIBLE_DATE = datetime.datetime(
-    year=2024, month=6, day=10, tzinfo=datetime.timezone.utc
-)
-RETWEET_HALT_DATE = datetime.datetime(
-    year=2024, month=7, day=6, tzinfo=datetime.timezone.utc
-)
