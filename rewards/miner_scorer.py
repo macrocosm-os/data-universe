982deb098568e620e160b8ed81f3c3dd2be8a5f2
chai-amy <amy.chai@macrocosmos.ai>
2025-01-28 17:54:51 +0000

    display float in logs instead of tensor

--- rewards/miner_scorer.py
+++ rewards/miner_scorer.py
@@ -192,7 +192,7 @@ class MinerScorer:
                 if dt.datetime.now(dt.timezone.utc) >= HF_REWARD_DATE:
                     # Awarding the miner their HF boost based on their last HF evaluation. 
                     score += self.hf_boosts[uid] * self.hf_credibility[uid]
-                    bt.logging.info(f"Awarded Miner {uid} a HF boost of {float(self.hf_boosts[uid] * self.hf_credibility[uid])} based off of the last performed HF evaluation, adjusting the score to {score}.")
+                    bt.logging.info(f"Awarded Miner {uid} a HF boost of {float(self.hf_boosts[uid] * self.hf_credibility[uid])} based off of the last performed HF evaluation, adjusting the score to {float(score)}.")
 
                 # Now update the credibility again based on the current validation results.
                 self._update_credibility(uid, validation_results)
