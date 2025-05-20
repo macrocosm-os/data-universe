04108623c4a5f48fa9ea8ecb82df1c127fd7d6fb
arrmlet <trubavolodymyr@gmail.com>
2025-05-20 20:58:20 +0300

    Change typo in the comment.

--- neurons/validator.py
+++ neurons/validator.py
@@ -422,7 +422,7 @@ class Validator:
                 self.evaluation_cycles_since_startup = 0
                 bt.logging.info("Initializing evaluation cycles counter for delayed weight setting")
 
-            # If we've completed fewer than 2 evaluation cycles, don't set weights
+            #  if we've completed fewer than the allotted number of evaluation cycles, don't set weights
             if self.evaluation_cycles_since_startup < constants.EVALUATION_ON_STARTUP:
                 bt.logging.info(
                     f"Skipping weight setting - completed {self.evaluation_cycles_since_startup}/15 evaluation cycles since startup")
