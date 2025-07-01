cd97ec0cf6eda0d32daa1da58d850cdc26fa80f0
chai-amy <amy.chai@macrocosmos.ai>
2025-07-01 22:11:40 +0000

    update organic validator whitelist

--- neurons/config.py
+++ neurons/config.py
@@ -169,8 +169,8 @@ def add_args(neuron_type: NeuronType, parser):
             "--organic_whitelist",
             nargs="+",
             help="Whitelist of hotkeys allowed for organic requests",
-            default=['5Cg5QgjMfRqBC6bh8X4PDbQi7UzVRn9eyWXsB8gkyfppFPPy', '5DZyu65U7AAWCD6UAKeovaVExAXksMjSX2J8YSqQmorkw3C1',
-                     '5F4tQyWrhfGVcNhoqeiNsR6KjD4wMZ2kfhLj4oHYuyHbZAc3'], #uids 89, 226, and 232
+            default=['5CzCCUzF3h5Eq3fNAr696YGRZvETYTnUHDcL16C3c9cpmbtE',
+                     '5HEU7ksVSKoCHY3SVcRkJYyGRKTUtKEfCDk5m5QgJUBA4F3F'], #uids 89 and 232
         )
 
         parser.add_argument(
