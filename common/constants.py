bb9187397cd92570123915419cbbeffc55ae5a39
arrmlet <trubavolodymyr@gmail.com>
2026-01-16 22:13:20 +0200

    resolve asyncio event loop thread-affinity issue in validator

--- common/constants.py
+++ common/constants.py
@@ -42,8 +42,8 @@ REDDIT_MEDIA_REQUIRED_DATE = dt.datetime(2025, 8, 7, tzinfo=dt.timezone.utc)  # 
 # Date after which backwards compatibility for nested X content format will be removed
 X_ENHANCED_FORMAT_COMPATIBILITY_EXPIRATION_DATE = dt.datetime(2025, 9, 30, tzinfo=dt.timezone.utc) # September 30, 2025 UTC
 
-# Date after which filename format validation is enforced (data_{YYYYMMDD_HHMMSS}_{count}_{16hex}.parquet)
-FILENAME_FORMAT_REQUIRED_DATE = dt.datetime(2026, 1, 18, tzinfo=dt.timezone.utc) # January 18, 2026 UTC
+# Date after which filename format validation is enforced (data_{YYYYMMDD_HHMMSS}_{count}_{16hex}.parquet). To be removed fully.
+FILENAME_FORMAT_REQUIRED_DATE = dt.datetime(2026, 2, 18, tzinfo=dt.timezone.utc)  # February 18, 2026 UTC
 
 EVALUATION_ON_STARTUP = 15
 
